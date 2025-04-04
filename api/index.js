7914f41fab737689da5830da19099256b03451da
NGUYEN MINH NHAT <nhatnguyenminh1507@gmail.com>
2025-04-04 17:11:00 +0700

    fix deploy

--- api/index.js
+++ api/index.js
@@ -8,10 +8,7 @@ const cookieParser= require('cookie-parser')
 // init middlewares
 app.use(cookieParser())
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}))
+app.use(cors())
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
